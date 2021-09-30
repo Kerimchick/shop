493ad6068ef9f58c58a5062b3d1d1484a4a7fa99
Kerimchick <47135315+Kerimchick@users.noreply.github.com>
2021-10-01 02:56:07 +0600

    fixed cart in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import {useSelector} from "react-redux";
-import Cart from "../../views/Cart";
 
 const Header = () => {
     const cart = useSelector((c) => c.cart)
