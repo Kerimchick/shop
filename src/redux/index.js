35b91d0f77508c3ac6e91e3a3eec6000cdfab00c
Kerimchick <47135315+Kerimchick@users.noreply.github.com>
2021-10-01 02:49:19 +0600

    add counter

--- src/redux/index.js
+++ src/redux/index.js
@@ -14,6 +14,13 @@ function reducer(state = {catalog: [], cart : []}, action) {
             return { ...state, cart: [...state.cart, {...action.payload, quantity : 1}]}
         case 'REMOVE_FROM_CART':
             return {...state, cart: state.cart.filter(el => el.id !== action.payload)}
+        case 'DECREASE_AMOUNT_IN_CART':
+            const deleteProduct = state.cart.find(el => el.id === action.payload.id)
+            if(deleteProduct){
+                return {...state, cart: state.cart.map(item => item.id === deleteProduct.id ?
+                        {...deleteProduct, quantity: Math.max(deleteProduct.quantity - 1, 1)} : item)}
+            }
+            return state
         default:
             return state
     }
--- src/views/Cart/index.js
+++ src/views/Cart/index.js
@@ -7,35 +7,47 @@ const Cart = () => {
     return (
         <div className="mt-3">
             {
-                cart.length ? <table className="table table-primary">
-                    <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Price</th>
-                        <th>Amount</th>
-                        <th>Sum</th>
-                        <th>Delete</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        cart.map(el =>
+                cart.length ?
+                    <>
+                        <table className="table table-primary">
+                            <thead>
                             <tr>
-                                <td>{el.title}</td>
-                                <td>{el.price}</td>
-                                <td>{el.quantity}</td>
-                                <td>{(el.price * el.quantity).toFixed(2)}</td>
-                                <td>
-                                    <button className="btn btn-danger"
-                                            onClick={() => dispatch({type: "REMOVE_FROM_CART", payload : el.id})}
-                                    >Delete</button>
-                                </td>
+                                <th>Name</th>
+                                <th>Price</th>
+                                <th>Amount</th>
+                                <th>Sum</th>
+                                <th>Delete</th>
                             </tr>
-                        )
-                    }
-                    </tbody>
-                    <h4>Total</h4>
-                </table> : "Cart is empty"
+                            </thead>
+                            <tbody>
+                            {
+                                cart.map(el =>
+                                    <tr>
+                                        <td>{el.title}</td>
+                                        <td>{el.price}</td>
+                                        <td>
+                                            <button className="btn-sm btn btn-danger me-1"
+                                                    onClick={() => dispatch({type: "DECREASE_AMOUNT_IN_CART", payload: el})}>-</button>
+                                            {el.quantity}
+                                            <button className="btn-sm btn btn-success ms-1"
+                                                    onClick={() => dispatch({type: "ADD_TO_CART", payload: el})}>+</button>
+                                        </td>
+                                        <td>{(el.price * el.quantity).toFixed(2)}</td>
+                                        <td>
+                                            <button className="btn btn-danger"
+                                                    onClick={() => dispatch({type: "REMOVE_FROM_CART", payload : el.id})}
+                                            >Delete</button>
+                                        </td>
+                                    </tr>
+                                )
+                            }
+                            </tbody>
+                        </table>
+                        <h4 className="text-end">Total: {cart.reduce((acc, item) => {
+                            return  (item.price * item.quantity) + acc
+                        }, 0).toFixed(2)}</h4>
+                    </>
+                : "Cart is empty"
             }
         </div>
     );
